5fefb66f78f85cb707511778f7aee0cdc09aaf42
Salim66 <salimhasanriad8@gmail.com>
2022-09-19 10:16:47 +0600

    Here is the basic state flow of Redux

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
+
+    const counter = useSelector( state => state );
+    const dispatch = useDispatch();
+
   return (
     <div className="container my-5">
         <div className="row justify-content-center overflow-hidden">
             <div className="col-md-5">
                 <div className="card counter shadow-sm">
                     <div className="card-body">
-                        <h1>0</h1>
-                        <button className='btn btn-success btn-sm'>++</button>
-                        <button className='btn btn-danger btn-sm'>--</button>
+                        <h1>{ counter }</h1>
+                        <button onClick={ () => dispatch({ type: 'INCREMENT' }) } className='btn btn-success btn-sm'>++</button> &nbsp;
+                        <button onClick={ () => dispatch({ type: 'DECREMENT' }) } className='btn btn-danger btn-sm'>--</button>
                     </div>
                 </div>
             </div>
---
+++ src/redux/counterReducer.js
@@ -0,0 +1,26 @@
+import { createStore } from "redux";
+
+
+// initial state
+let initialState = 0;
+
+
+// create counter reducer
+const counterReducer = (state = initialState, { type, payload }) => {
+
+    switch (type) {
+        case 'INCREMENT':
+            return state + 1;
+            break;
+        case 'DECREMENT':
+            return state - 1;
+            break;
+
+        default:
+            return state;
+    }
+
+}
+
+// create redux store
+export const store = createStore(counterReducer);
